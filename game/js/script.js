52862ec3ad3fbc21b0d8a370ec8b2aea9f7a1ae0
EduardaMendes01 <eduardamendes0100@gmail.com>
2024-03-23 23:16:40 -0300

    game update

--- game/js/script.js
+++ game/js/script.js
@@ -1,8 +1,10 @@
 const me = document.querySelector('.me');
 const rock = document.querySelector('.rock');
+const him = document.querySelector('.him');
 
 let level = 1;
 let score = 0;
+let stonesSkipped = 0;
 
 const jump = () => {
     me.classList.add('jump');
@@ -11,6 +13,7 @@ const jump = () => {
         me.classList.remove('jump');
     }, 500)
 }
+
 
 const loop = setInterval( () => {
     const rockPosition = rock.offsetLeft;
@@ -30,7 +33,8 @@ if(rockPosition <= 115 && rockPosition > 0 && mePosition < 60){
     gameOver();
 
 } else if(rockPosition <= 0){
-    score++;
+    stonesSkipped++;
+    score += 1;
     updateScore();
 }
 
@@ -41,6 +45,37 @@ if(rockPosition <= 115 && rockPosition > 0 && mePosition < 60){
 const updateScore = () => {
     const scoreElement = document.getElementById('score');
     scoreElement.textContent = `Score: ${score}`;
+
+    if(score % 30 === 0 && score > 0){
+        level = score / 30 + 1;
+        him.style.display = 'block';
+        me.stryle.visibility = 'hidden';
+        nextLevel();
+    }
+}
+
+//Collision me and him
+const checkCollision = () => {
+    const meRect = me.getBoundingClientRect();
+    const himRect = him.getBoundingClientRect();
+
+    if (
+        meRect.left < himRect.right &&
+        meRect.right > himRect.left &&
+        meRect.top < himRect.bottom &&
+        meRect.bottom > himRect.top
+    ) {
+        // 'me' and 'him' characters collide, handle collision logic here
+        // For example, don't lose the game
+    }
+}
+
+// Function to transition to the next level
+const nextLevel = () => {
+    const levelElement = document.getElementById('level');
+    levelElement.textContent = `Level: ${level}`;
+     // Increment level
+    // Reset game elements, adjust game parameters, etc. for the next level
 }
 
 document.addEventListener('keydown', jump);
